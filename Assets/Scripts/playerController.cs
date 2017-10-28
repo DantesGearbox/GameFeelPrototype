072a26b0f960a5938abb2e7a16dcd7ea779f4f53
DantesGearbox <mads.engberg@hotmail.com>
2017-10-28 21:13:39 +0200

    Inital commit

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	//Components
+	Rigidbody2D rb;
+
+	//Outer variables
+
+	//Inner variables
+	private int jumpSpeed = 40;
+	private int minJumpSpeed = 10;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown (KeyCode.Space)){
+			rb.velocity += new Vector2 (0, jumpSpeed);
+		}
+
+		if (Input.GetKeyUp (KeyCode.Space) && rb.velocity.y > minJumpSpeed) {
+			rb.velocity = new Vector2 (rb.velocity.x, minJumpSpeed);
+		}
+
+
+
+	}
+}
