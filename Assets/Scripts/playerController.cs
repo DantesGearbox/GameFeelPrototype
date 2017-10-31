4b5303c17157443161a216381136e07e49d90269
Mads <mads.engberg@hotmail.com>
2017-10-31 01:33:34 +0100

    Still doing animations

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -2,34 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
-	//Components
-	Rigidbody2D rb;
-
-	//Outer variables
-
-	//Inner variables
-	private int jumpSpeed = 40;
-	private int minJumpSpeed = 10;
-
+	private Rigidbody2D rb;
+	private bool jumpPressed = false;
+	private bool jumpLetGo = false;
+	public Vector2 jumpVector = new Vector2 (0, 40); 
+	public Vector2 airJumpVector = new Vector2 (0, 40);
+	public KeyCode jumpKey = KeyCode.Space;
+	public string horizontalAxis = "Horizontal";
+	public string verticalAxis = "Vertical";
+	public int minJumpVelocity = 10;
+	private float directionalInput;
+	public int moveSpeed = 12;
+	private bool inAir = false;
+	public int airJumps = 1;
+	//public int airSpecials = 1;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
 	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		inAir = false;
+		airJumps = 1;
+	}
+
+	void OnCollisionExit2D(Collision2D coll){
+		inAir = true;
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown (KeyCode.Space)){
-			rb.velocity += new Vector2 (0, jumpSpeed);
+
+		if(Input.GetKeyDown (jumpKey)){
+			jumpPressed = true;
 		}
 
-		if (Input.GetKeyUp (KeyCode.Space) && rb.velocity.y > minJumpSpeed) {
-			rb.velocity = new Vector2 (rb.velocity.x, minJumpSpeed);
+		if(Input.GetKeyUp (jumpKey)){
+			jumpLetGo = true;
 		}
 
+		directionalInput = Input.GetAxisRaw (horizontalAxis);
 
+	}
 
+	//Also called at a specific interval
+	void FixedUpdate(){
+
+		float targetVelocityX = directionalInput * moveSpeed;
+		rb.velocity = new Vector2 (targetVelocityX , rb.velocity.y);	
+
+		if(jumpPressed){
+			rb.velocity = Vector2.zero;
+			if(!inAir){
+				rb.velocity += jumpVector;
+			} else if(inAir && airJumps > 0){
+				rb.velocity += airJumpVector;
+				airJumps--;
+			}
+			jumpPressed = false;
+		}
+
+		if(jumpLetGo){
+			if(rb.velocity.y > minJumpVelocity){
+				rb.velocity = new Vector2(rb.velocity.x, minJumpVelocity);
+			}
+			jumpLetGo = false;
+		}
 	}
 }
